a6a817d04bcf8ad89a7af5ef6abd95131319af15
Stefan Dörger <stefan.doerger@winlocal.de>
2020-04-20 15:10:01 +0200

    Cleaned Up components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
 
-      <message></message>
+    <message>Hi There</message>
+    <counter></counter>
 
   </div>
 </template>
 
 <script>
+
+import Message from "./components/Message.vue";
+import Counter from "./components/Counter.vue";
+
 export default {
   name: 'app',
+
+  components: {Message, Counter},
+
   data () {
     return {
       msg: 'Welcome to Your Vue.js App, by sdDev'
